937414aec904968bc68dc47d5f98e84d5f6ed22e
wotusay <wout.salembier@hotmail.com>
2020-02-23 14:12:05 +0100

    added all the tests

--- src/js/Comment.js
+++ src/js/Comment.js
@@ -1,17 +1,27 @@
 import {decorate, observable, action} from 'mobx';
 
 class Comment {
-  constructor({content, user, likes, picture, key, liked = false}) {
+  constructor({content, user, likes = 0, picture, key, liked = false}) {
     this.content = content;
     this.user = user;
     this.likes = likes;
     this.picture = picture;
     this.key = key;
     this.liked = liked;
+
+    if (!picture) {
+      throw 'You need an picture to comment!';
+    }
+
+    if (!content) {
+      throw 'You need content for your comment!';
+    }
   }
-  //For the liked picture
+
+  //For the liked comment
   setCommentLiked() {
     this.liked = true;
+    this.likes ++;
   }
 }
 
--- src/js/Comment.test.js
+++ src/js/Comment.test.js
@@ -1,14 +1,25 @@
 import Comment from './Comment';
 
 test('New Comment', () => {
-  const comment = new Comment({content: 'test', user: 'BigBoi'});
+  const comment = new Comment({content: 'test', user: 'BigBoi', picture: 1});
   expect(comment.content).not.toBe('');
   expect(comment.user).not.toBe('');
 });
 
 test('Liking the comment', () => {
-  const c = new Comment();
-  expect(c.length).toBe(0);
-  
+  const c = new Comment(({content: 'test', user: 'test', picture: 1}));
+  expect(c.likes).toBe(0);
+  expect(c.liked).toBe(false);
+  c.setCommentLiked();
+  expect(c.likes).toBe(1);
+  expect(c.liked).toBe(true);
 });
 
+test('Picture must have an name', () => {
+  expect (() => new Comment({content: 'Test', user: 'test', key: 0})).toThrow();
+});
+
+test('Comment must have content', () => {
+  expect(() => new Comment({picture: {}})).toThrow();
+  expect(() => new Comment({content: '', picture: {}})).toThrow();
+});
